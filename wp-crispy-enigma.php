2e2e4d59497597f335439bac894f798b576d7ad0
Cowan <cowanr@ecu.edu>
2019-04-15 23:24:30 -0400

    add unit tests

--- wp-crispy-enigma.php
+++ wp-crispy-enigma.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Plugin Name: Simple ToDo1
+Plugin Name: Simple ToDo
 Plugin URI:  https://developer.wordpress.org/plugins/the-basics/
 Description: A Basic WordPress Plugin for illustrating CI and unit testing.   Creates a custom post type for todo items.
 Version:     1.0
